48f5adeab9da4e3ee1ac2360808b009dab118ad2
Jeremieon <115072636+Jeremieon@users.noreply.github.com>
2022-11-26 19:58:59 +0100

    fixed urls

--- Rest_Api/urls.py
+++ Rest_Api/urls.py
@@ -1,7 +1,8 @@
+from quickstart import views
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
-from rest_framework.quickstart import views
+
 
 router = routers.DefaultRouter()
 router.register(r'users',views.UserViewSet)
@@ -11,6 +12,7 @@ router.register(r'groups',views.GroupViewSet)
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    #path("admin/", admin.site.urls),
+    path('', include(router.urls)),
     path('api-auth/',include('rest_framework.urls',namespace='rest_framework'))
 ]
--- quickstart/views.py
+++ quickstart/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User,Group
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from Rest_Framework.quickstart.serializers import UserSerializer,GroupSerializer
+from quickstart.serializers import UserSerializer, GroupSerializer
 
-
-
-# Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = UserViewSet.objects.all().order_by('-date_joined')
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
 class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
